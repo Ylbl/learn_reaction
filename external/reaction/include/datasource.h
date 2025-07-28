d4939476b85e612d386ef1e7041cc5e34e41b151
Administrator <1278309552@qq.com>
2025-07-28 23:37:10 +0800

    #

--- external/reaction/include/datasource.h
+++ external/reaction/include/datasource.h
@@ -9,23 +9,31 @@
 
 namespace reaction
 {
-    class Datasource
+    template <class Type, class... Args>
+    class Datasource : public Expression<Type, Args...>
     {
-        public:
-            auto get()
-            {
-
-            }
+    public:
+        using Expression<Type, Args...>::Expression;
+/**
+* @brief ...
+* TODO const用法?
+*/
+        auto get() const
+        {
+            return this->get_value();
+        }
     };
 
-    template<class T>
-    auto var(T &&t)
+    template <class Type> auto var(Type&& t)
     {
-        return Datasource();
+        return Datasource<Type>(std::forward<Type>(t));
     }
 
-    template<class F, class... Args>
-    auto calc(F &&f, Args &&... args) {}
+    template <class Fun, class... Args> auto calc(Fun&& fun, Args&&... args)
+    {
+        return Datasource<std::decay_t<Fun>, std::decay_t<Args>...>
+            (std::forward<Fun>(fun), std::forward<Args>(args)...);
+    }
 } // namespace reaction
 
 #endif  // DATASOURCE_H
